db731862d3ab54d956f54167e600be4fd1672be7
Joshua Stolle <joshuastolle445@gmail.com>
2024-08-18 19:30:11 +0200

    feat: Skeleton Card for init app loading state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import AppContent from '@/components/AppContent';
 import { useStore } from '@/hooks/useStore';
 import { useEffect } from 'react';
 import { Toaster } from '@/components/ui/toaster';
+import SkeletonCard from '@/components/SkeletonCard';
 
 export default function Home() {
   const initPersons = useStore((state) => state.initPersons);
@@ -16,8 +17,9 @@ export default function Home() {
   return (
     <>
       <main className="bg-background text-foreground h-screen flex flex-col max-w-[800px] mx-auto w-full">
-        {loading ? <h2>loading</h2> : <AppContent />}
+        {loading ? <SkeletonCard /> : <AppContent />}
       </main>
+
       <Toaster />
     </>
   );
