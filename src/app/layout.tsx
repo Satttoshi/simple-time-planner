6edc84d7d636f350edb039db594aa7812160d6ea
Joshua Stolle <joshuastolle445@gmail.com>
2025-03-08 10:58:54 +0100

    feat: update title to 'Reveal TACTX Planner'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'venary time planner',
+  title: 'Reveal TACTX Planner',
   description: 'weekly planning for teammates',
 };
 
